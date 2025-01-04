e6d95e66e72d778d2d0134bae06a0030dad42d27
Ratnakar <ratnakargiri973@gmail.com>
2025-01-04 20:41:30 +0530

    frontend url added in backend

--- Server/index.js
+++ Server/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 const corsOption = {
-    origin: "http://localhost:5173",
+    origin: "https://thread-project-1.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
