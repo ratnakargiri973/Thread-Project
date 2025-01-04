7ebe0ab8178f02b57ed0a8494a3c0a80e66bdf86
Ratnakar <ratnakargiri973@gmail.com>
2025-01-04 20:37:01 +0530

    dist added

--- Client/Thread-Frontend/src/axiosConfig.js
+++ Client/Thread-Frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:2001/api/v1",
+    baseURL: "https://thread-project.onrender.com/api/v1",
     withCredentials: true
 })
 
